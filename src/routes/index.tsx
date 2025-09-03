1c016b83c5d65ceebf5b84b1684c62af0fc9c149
Imranahmed935 <imrantahir9918@gmail.com>
2025-09-03 10:38:12 +0600

    user created successfully by  react redux

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,8 @@
 import App from "@/App";
+import { LoginForm } from "@/components/Authentication/LoginForm";
+import { RegisterForm } from "@/components/Authentication/RegisterForm";
 import About from "@/pages/About";
+import Verify from "@/pages/verify";
 
 import { createBrowserRouter } from "react-router";
 
@@ -10,8 +13,21 @@ export const router = createBrowserRouter([
     children: [
       {
         Component: About,
-        path: "about",
+        path: "/about",
       },
     ],
   },
+  {
+    Component: LoginForm,
+    path: "/login",
+  },
+  {
+    Component: RegisterForm,
+    path: "/register",
+  },
+  {
+    Component: Verify,
+    path: "/verify",
+  },
+  
 ]);
